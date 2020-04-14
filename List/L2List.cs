73b4c25c7c732ff8ec208995af2e2d9978846aaa
Oleg <olegshitikov@gmail.com>
2020-04-14 16:46:22 +0300

    Добавил класс Canvas и сопутствующие ему.

---
+++ List/L2List.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace risovalka.List
+{
+    class L2List
+    {
+    }
+}
