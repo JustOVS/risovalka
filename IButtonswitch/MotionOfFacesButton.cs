01da628671ad41685e18142ba78eeb8ce445f55a
Oleg <olegshitikov@gmail.com>
2020-05-01 18:57:15 +0300

    доработка движения грани прямоугольника

--- IButtonswitch/MotionOfFacesButton.cs
+++ IButtonswitch/MotionOfFacesButton.cs
@@ -21,8 +21,8 @@ namespace risovalka.IButtonswitch
 
         public override bool ActivateButton(Point p1, PictureBox pictureBox, ref Color currentColor, ref AbstractPainter currentPainter)
         {
-            tmpPoint = p1;
-            currentPainter = Canvas.GetCanvas.FindFigureByPoint1(tmpPoint);
+            
+            currentPainter = Canvas.GetCanvas.FindFigureByPoint1(p1, ref tmpPoint);
             tmpIndex = Canvas.GetCanvas.FindPointByPoint(tmpPoint);
             ChangingFlag = true;
             return false;
@@ -33,20 +33,44 @@ namespace risovalka.IButtonswitch
         {
             if (ChangingFlag && currentPainter != null)
             {
-                Point p2 = currentPainter.points[tmpIndex + 1];
 
+                Point p2; // = tmpIndex+1 != currentPainter.points.Count? currentPainter.points[tmpIndex + 1] : currentPainter.points[0];
+                int p2Index;
+                if(tmpIndex + 1 < currentPainter.points.Count)
+                {
+                    p2 = currentPainter.points[tmpIndex + 1];
+                    p2Index = tmpIndex + 1;
+                }
+                else
+                {
+                    p2 = currentPainter.points[0];
+                    p2Index = 0;
+                }
                 //Point p1New = p1 + dx;
-                //Point p2New = p2 + dy;
-                p1.X = tmpPoint.X + p1.X;
-                p1.Y  = tmpPoint.Y  + p1.Y ; 
-                p2.X = tmpPoint.X + p2.X;
-                p2.Y  = tmpPoint.Y  + p2.Y ;
+                ////Point p2New = p2 + dy;
+                //p1.X = tmpPoint.X + p1.X;
+                //p1.Y  = tmpPoint.Y  + p1.Y ; 
+                //p2.X = tmpPoint.X + p2.X;
+                //p2.Y  = tmpPoint.Y  + p2.Y ;
+
+                if( tmpPoint.X == p2.X)
+                {
+                    int dx = p1.X - tmpPoint.X;
+                    currentPainter.points[tmpIndex] = new Point(tmpPoint.X+ dx, tmpPoint.Y);
+                    currentPainter.points[p2Index] = new Point(p2.X + dx, p2.Y);
+                }
+                else if (tmpPoint.Y == p2.Y)
+                {
+                    int dy = p1.Y = tmpPoint.Y;
+                    currentPainter.points[tmpIndex] = new Point(tmpPoint.X, tmpPoint.Y+dy);
+                    currentPainter.points[p2Index] = new Point(p2.X, p2.Y+dy);
+                }
 
 
 
 
-                tmpPoint = p1;
-                currentPainter.points[tmpIndex] = p1;
+                //tmpPoint = p1;
+                //currentPainter.points[tmpIndex] = p1;
 
                 Canvas.GetCanvas.DrawAllFigures(pictureBox);
             }
