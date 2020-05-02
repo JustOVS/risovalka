b38733f74141dc4e7b2d2f49ee42f54b30520e22
Tanya <zvereva_tatiana@icloud.com>
2020-05-02 21:59:17 +0700

    быстро находит линию, но пока не двигает

--- IButtonswitch/MotionOfFacesButton.cs
+++ IButtonswitch/MotionOfFacesButton.cs
@@ -11,56 +11,91 @@ using System.Windows.Forms;
 
 namespace risovalka.IButtonswitch
 {
-    class MotionOfFacesButton: IButton
+    class MotionOfFacesButton : IButton
     {
         Point tmpPoint;
         //Point tmpX;
         //Point tmpY;
         bool ChangingFlag = false;
         //int tmpIndex;
-        
-       
+
+
 
         public override bool ActivateButton(Point p1, PictureBox pictureBox, ref Color currentColor, ref AbstractPainter currentPainter)
         {
-            
+
             currentPainter = Canvas.GetCanvas.FindFigureByPoint1(p1, ref tmpPoint);
             //tmpIndex = Canvas.GetCanvas.FindPointByPoint(tmpPoint);
             ChangingFlag = true;
             return false;
 
-            
         }
 
 
         public override void Move(Point p1, PictureBox pictureBox, AbstractPainter currentPainter)
         {
-            
+
 
             if (ChangingFlag && currentPainter != null)
             {
-                for (int i=0; i<currentPainter.points.Count-2; i++)
+                Point checkPoint = currentPainter.points[0];
+                double minSum = 100000000000;
+               
+
+                for (int i = 0; i < currentPainter.points.Count - 1; i++)
                 {
-                    Point first = currentPainter.points[i];
-                    int j = i + 1;
-                    while (j< currentPainter.points.Count - 1)
+                    if (Math.Abs((currentPainter.points[i].X + currentPainter.points[i].Y) - (tmpPoint.X + tmpPoint.Y)) < minSum)
+
                     {
-                        Point second = currentPainter.points[j];
+                        minSum = Math.Abs((currentPainter.points[i].X + currentPainter.points[i].Y) - (tmpPoint.X + tmpPoint.Y));
+                        checkPoint = currentPainter.points[i];
+                    }
+                    
+                }
+               // checkPoint = currentPainter.points[_i];
 
-                        double A = second.Y - first.Y;
-                        double B = first.X - second.X;
-                        double C = second.X * first.Y - first.X * second.Y;
+                for (int i=0; i<currentPainter.points.Count-1; i++)
+                {
+                    double A = currentPainter.points[i].Y - checkPoint.Y;
+                    double B = checkPoint.X - currentPainter.points[i].X;
+                    double C = currentPainter.points[i].X * checkPoint.Y - checkPoint.X * currentPainter.points[i].Y;
+                    
 
-                        if (tmpPoint.X*A+tmpPoint.Y*B+C==0)
-                        {
-                            pictureBox.BackColor = Color.Red;
-                            currentPainter.points[i] = new Point(first.X+50, first.Y);
-                            currentPainter.points[j] = new Point(second.X+50, second.Y);
-                        }
-                        j++;
+                    if ((tmpPoint.X * A + tmpPoint.Y * B + C < 3) && (tmpPoint.X * A + tmpPoint.Y * B + C > -3))
+                    {
+
+                        pictureBox.BackColor = Color.Red;
+
+                        checkPoint = new Point();
+                        currentPainter.points[i] = new Point();
+
+                        //checkPoint = new Point(checkPoint.X+Math.Abs(checkPoint.X - tmpPoint.X), checkPoint.Y + Math.Abs(checkPoint.Y - tmpPoint.Y));
+                        // currentPainter.points[i] = new Point(currentPainter.points [i].X+ Math.Abs( currentPainter.points[i].X - tmpPoint.X), currentPainter.points[i].Y + Math.Abs(currentPainter.points[i].Y - tmpPoint.Y));
                     }
 
+
                 }
+                //{
+                //    Point first = currentPainter.points[i];
+                //    int j = i + 1;
+                //    while (j< currentPainter.points.Count - 1)
+                //    {
+                //        Point second = currentPainter.points[j];
+
+                //        double A = second.Y - first.Y;
+                //        double B = first.X - second.X;
+                //        double C = second.X * first.Y - first.X * second.Y;
+
+                //        if (tmpPoint.X*A+tmpPoint.Y*B+C==0)
+                //        {
+                //            pictureBox.BackColor = Color.Red;
+                //            currentPainter.points[i] = new Point(first.X+50, first.Y);
+                //            currentPainter.points[j] = new Point(second.X+50, second.Y);
+                //        }
+                //        j++;
+                //    }
+
+                //}
 
                 //Point p2; // = tmpIndex+1 != currentPainter.points.Count? currentPainter.points[tmpIndex + 1] : currentPainter.points[0];
                 //int p2Index;
@@ -81,8 +116,8 @@ namespace risovalka.IButtonswitch
                 //p2.X = tmpPoint.X + p2.X;
                 //p2.Y  = tmpPoint.Y  + p2.Y ;
 
-                
-                
+
+
                 //if (k>0)
                 //{
                 //    int d= Math.Abs(b-)
@@ -120,10 +155,11 @@ namespace risovalka.IButtonswitch
 
                 //tmpPoint = p1;
                 //currentPainter.points[tmpIndex] = p1;
-
-                Canvas.GetCanvas.DrawAllFigures(pictureBox);
             }
+
+            Canvas.GetCanvas.DrawAllFigures(pictureBox);
         }
+    
 
 
         public override void DeactivateButton()
@@ -131,4 +167,5 @@ namespace risovalka.IButtonswitch
             ChangingFlag = false;
         }
     }
+
 }
